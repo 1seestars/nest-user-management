48a9efb54a5f9b17ef489710f7a68b9f05ab217c
1seestars <vitalik.matyah@gmail.com>
2024-03-25 17:20:17 +0200

    chore(user-management: refactor the app

---
+++ src/users/user.model.ts
@@ -0,0 +1,38 @@
+import { Column, DataType, Model, Table } from 'sequelize-typescript';
+import { UserCreationAttributes } from './user.types';
+
+@Table({ tableName: 'users' })
+export class User extends Model<User, UserCreationAttributes> {
+  @Column({
+    type: DataType.INTEGER,
+    unique: true,
+    autoIncrement: true,
+    primaryKey: true,
+  })
+  id: number;
+
+  @Column({
+    type: DataType.STRING,
+    unique: true,
+    allowNull: false,
+  })
+  email: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  password: string;
+
+  @Column({
+    type: DataType.BOOLEAN,
+    defaultValue: false,
+  })
+  banned: boolean;
+
+  @Column({
+    type: DataType.BOOLEAN,
+    allowNull: true,
+  })
+  banReason: string;
+}
