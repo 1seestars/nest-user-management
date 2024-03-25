06c9c3db26b7ea3e42dc80e54944887b6933b57a
1seestars <vitalik.matyah@gmail.com>
2024-03-25 15:06:48 +0200

    feat(nvm): add nvmrc file

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 @Controller('/api')
 export class AppController {
   constructor(private appService: AppService) {}
+
   @Get('/users')
   getUsers() {
     return this.appService.getUsers();
